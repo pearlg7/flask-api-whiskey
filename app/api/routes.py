3911e424f0978a0fb46ffed4ab4ff04781d755c1
pearl <pearlgonzalez77@gmail.com>
2023-04-02 16:33:44 -0500

    first commit

---
+++ app/api/routes.py
@@ -0,0 +1,67 @@
+from flask import Blueprint, request, jsonify, render_template
+from helpers import token_required
+from models import db, User, Whiskey, whiskey_schema, whiskeys_schema
+
+api = Blueprint('api',__name__, url_prefix='/api')
+
+@api.route('/whiskeys', methods = ['POST'])
+@token_required
+def create_whiskey(current_user_token):
+    name = request.json['name']
+    style = request.json['style']
+    age = request.json['age']
+    user_token = current_user_token.token
+
+    print(f'BIG TESTER: {current_user_token.token}')
+
+    whiskey = Whiskey(name, style, age, user_token = user_token )
+
+    db.session.add(whiskey)
+    db.session.commit()
+
+    response = whiskey_schema.dump(whiskey)
+    return jsonify(response)
+
+@api.route('/whikeys', methods = ['GET'])
+@token_required
+def get_whiskey(current_user_token):
+    a_user = current_user_token.token
+    whiskeys = Whiskey.query.filter_by(user_token = a_user).all()
+    response = whiskeys_schema.dump(whiskeys)
+    return jsonify(response)
+
+@api.route('/whiskeys/<id>', methods = ['GET'])
+@token_required
+def get_whiskey_two(current_user_token, id):
+    fan = current_user_token.token
+    if fan == current_user_token.token:
+        whiskey = Whiskey.query.get(id)
+        response = whiskey_schema.dump(whiskey)
+        return jsonify(response)
+    else:
+        return jsonify({"message": "Valid Token Required"}),401
+
+# UPDATE endpoint
+@api.route('/whiskeys/<id>', methods = ['POST','PUT'])
+@token_required
+def update_whiskey(current_user_token,id):
+    whiskey = Whiskey.query.get(id) 
+    whiskey.name = request.json['name']
+    whiskey.style = request.json['style']
+    whiskey.age = request.json['age']
+    whiskey.user_token = current_user_token.token
+
+    db.session.commit()
+    response = whiskey_schema.dump(whiskey)
+    return jsonify(response)
+
+
+# DELETE whiskey ENDPOINT
+@api.route('/whiskeys/<id>', methods = ['DELETE'])
+@token_required
+def delete_whiskey(current_user_token, id):
+    whiskey = Whiskey.query.get(id)
+    db.session.delete(whiskey)
+    db.session.commit()
+    response = whiskey_schema.dump(whiskey)
+    return jsonify(response)
